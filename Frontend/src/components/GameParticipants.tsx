860ea103108e60fc717f78550fdeb0b358298128
relic <relicdragon@yandex.ru>
2025-11-30 21:29:52 +0100

    hm

--- Frontend/src/components/GameParticipants.tsx
+++ Frontend/src/components/GameParticipants.tsx
@@ -105,7 +105,7 @@ export const GameParticipants = ({
             {t('createGame.addMeToGame')}
           </Button>
         )}
-        <div className="flex flex-wrap gap-4">
+        <div className="flex flex-wrap gap-6">
           {game.participants.filter(p => p.isPlaying).map((participant) => (
             <PlayerAvatar
               key={participant.userId}
