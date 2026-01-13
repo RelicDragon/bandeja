69f60cb646c1df8a76232bf9e804b473185bf902
relic <relicdragon@yandex.ru>
2026-01-13 23:52:14 +0100

    Update game meta tags for improved SEO and social sharing
    - Modified the `generateGameMetaTags` function to change the Twitter card type from "summary_large_image" to "summary".
    - Removed the Twitter image meta tag and the Open Graph image meta tag to streamline metadata for better performance.

--- Backend/src/controllers/metatags.controller.ts
+++ Backend/src/controllers/metatags.controller.ts
@@ -84,12 +84,10 @@ const generateGameMetaTags = (game: any): string => {
     <meta property="og:locale" content="en_US">
     <meta property="og:title" content="${title}">
     <meta property="og:description" content="${description}">
-    <meta property="og:image" content="${imageUrl}">
     
-    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:card" content="summary" />
     <meta name="twitter:title" content="${title}" />
     <meta name="twitter:description" content="${description}" />
-    <meta name="twitter:image" content="${imageUrl}" />
     
     <meta name="description" content="${description}" />
     
